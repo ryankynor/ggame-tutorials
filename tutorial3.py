7f73d7d9c060dc96902f9b1d0b7dbed83f06faeb
ryankynor <ryankynor@hanovernorwichschools.org>
2015-10-13 14:16:55 -0400

    Updated from Brython Server: 10/13/2015 2:16:55 PM

--- tutorial3.py
+++ tutorial3.py
@@ -7,3 +7,18 @@ from ggame import LineStyle, Color, Sprite, Sound
 
 myapp = App()
 myapp.run()
+
+
+SCREEN_WIDTH = 640
+SCREEN_HEIGHT = 480
+
+
+green = Color(0x00ff00, 1)
+black = Color(0, 1)
+
+
+noline = LineStyle(0, black)
+
+
+bg_asset = RectangleAsset(SCREEN_WIDTH, SCREEN_HEIGHT, noline, green)
+bg = Sprite(bg_asset, (0,0))
